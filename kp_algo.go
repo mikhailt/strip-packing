e454d18a24a508fc6e5ed327f70807631bf6fbf4
mikhailt <mikhailt@tratata.local>
2010-12-19 17:45:34 +0300

    gofmt

--- kp_algo.go
+++ kp_algo.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "cmath"
-  "fmt"
+	"cmath"
+	"fmt"
 )
 
 type Bin struct {
-  Rect
-  top float64
-  t int
+	Rect
+	top float64
+	t   int
 }
 
 // Packs rectangles from slice 'a' to the strip starting from lower bound 'be'
@@ -16,101 +16,101 @@ type Bin struct {
 // Returns an upper bound of resulting alignment. This algo in not quite 
 // on-line, it uses number of all the rectangles.
 type Kp1Algo struct {
-  frame Bin
-  bins []Bin
-  nbins int
-  delta, u float64
-  d int
+	frame    Bin
+	bins     []Bin
+	nbins    int
+	delta, u float64
+	d        int
 }
 
 func (v *Kp1Algo) Pack(a []Rect, be float64) float64 {
-  v.frame.top = 0
-  v.frame.y = be
-  v.frame.x = 0
-  v.frame.w = 1
-  
-  n := len(a)
-  v.bins = make([]Bin, n)
-  v.nbins = 0
-  var j int
-  var r *Rect
-  
-  v.delta = real(cmath.Pow(cmplx(float64(n), 0), (-1.0/3)))
-  v.u = real(cmath.Pow(cmplx(float64(n), 0), (1.0/3)))
-  v.d = int(1 / (2 * v.delta))
-  
-  fmt.Printf("delta = %0.9v\nu = %0.9v\n", v.delta, v.u)
-  
-  for i, _ := range(a) {
-    r = &a[i]
-    if r.w > (1 - v.delta) {
-      PackToBin(&v.frame, r)
-      continue
-    }
-    // Determining type of the current rectangle.
-    j = 0
-    for y := 1; y <= v.d; y++ {
-      if r.w <= (v.delta * float64(y)) {
-        j = y
-        break
-      }
-    }
-    if 0 == j {
-      for y := v.d; y >= 1; y-- {
-        if r.w <= (1 - v.delta * float64(y)) {
-          j = v.d * 2 - y + 1
-          break
-        }
-      }
-    }
-    // Finding suitable opened bin for current rectangle.
-    bin_found := false
-    for y := 0; y < v.nbins; y++ {
-      if v.bins[y].t != j {
-        continue
-      }
-      if (v.bins[y].h - v.bins[y].top) >= r.w {
-        PackToBin(&v.bins[y], r)
-        bin_found = true
-        break
-      }
-    }
-    if bin_found {
-      continue
-    }
-    // Opening pair of new bins and packing current rectangle into corresponging
-    // one.
-    v.AddBin(j)
-    PackToBin(&v.frame, &v.bins[v.nbins - 1].Rect)
-    PackToBin(&v.bins[v.nbins - 1], r)
-    v.AddBin(v.ComplType(j))
-    v.bins[v.nbins - 1].y = v.bins[v.nbins - 2].y
-    v.bins[v.nbins - 1].x = v.bins[v.nbins - 2].w
-  }
-  return v.frame.y + v.frame.top
+	v.frame.top = 0
+	v.frame.y = be
+	v.frame.x = 0
+	v.frame.w = 1
+
+	n := len(a)
+	v.bins = make([]Bin, n)
+	v.nbins = 0
+	var j int
+	var r *Rect
+
+	v.delta = real(cmath.Pow(cmplx(float64(n), 0), (-1.0 / 3)))
+	v.u = real(cmath.Pow(cmplx(float64(n), 0), (1.0 / 3)))
+	v.d = int(1 / (2 * v.delta))
+
+	fmt.Printf("delta = %0.9v\nu = %0.9v\n", v.delta, v.u)
+
+	for i, _ := range a {
+		r = &a[i]
+		if r.w > (1 - v.delta) {
+			PackToBin(&v.frame, r)
+			continue
+		}
+		// Determining type of the current rectangle.
+		j = 0
+		for y := 1; y <= v.d; y++ {
+			if r.w <= (v.delta * float64(y)) {
+				j = y
+				break
+			}
+		}
+		if 0 == j {
+			for y := v.d; y >= 1; y-- {
+				if r.w <= (1 - v.delta*float64(y)) {
+					j = v.d*2 - y + 1
+					break
+				}
+			}
+		}
+		// Finding suitable opened bin for current rectangle.
+		bin_found := false
+		for y := 0; y < v.nbins; y++ {
+			if v.bins[y].t != j {
+				continue
+			}
+			if (v.bins[y].h - v.bins[y].top) >= r.w {
+				PackToBin(&v.bins[y], r)
+				bin_found = true
+				break
+			}
+		}
+		if bin_found {
+			continue
+		}
+		// Opening pair of new bins and packing current rectangle into corresponging
+		// one.
+		v.AddBin(j)
+		PackToBin(&v.frame, &v.bins[v.nbins-1].Rect)
+		PackToBin(&v.bins[v.nbins-1], r)
+		v.AddBin(v.ComplType(j))
+		v.bins[v.nbins-1].y = v.bins[v.nbins-2].y
+		v.bins[v.nbins-1].x = v.bins[v.nbins-2].w
+	}
+	return v.frame.y + v.frame.top
 }
 
 func (v *Kp1Algo) AddBin(t int) {
-  v.bins[v.nbins].h = v.u
-  v.bins[v.nbins].w = v.WidthType(t)
-  v.bins[v.nbins].top = 0
-  v.bins[v.nbins].t = t
-  v.nbins++
+	v.bins[v.nbins].h = v.u
+	v.bins[v.nbins].w = v.WidthType(t)
+	v.bins[v.nbins].top = 0
+	v.bins[v.nbins].t = t
+	v.nbins++
 }
 
 func PackToBin(bin *Bin, r *Rect) {
-  r.x = bin.x
-  r.y = bin.y + bin.top
-  bin.top += r.h
+	r.x = bin.x
+	r.y = bin.y + bin.top
+	bin.top += r.h
 }
 
 func (v *Kp1Algo) ComplType(t int) int {
-  return 2 * v.d - t + 1
+	return 2*v.d - t + 1
 }
 
 func (v *Kp1Algo) WidthType(t int) float64 {
-  if t <= v.d {
-    return v.delta * float64(t)
-  }
-  return 1 - float64(v.ComplType(t)) * v.delta
+	if t <= v.d {
+		return v.delta * float64(t)
+	}
+	return 1 - float64(v.ComplType(t))*v.delta
 }
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
-package main 
+package main
 
 import (
-  "gdk"
-  "gtk"
-  "os"
-  "rand"
-  "time"
-  "fmt"
+	"gdk"
+	"gtk"
+	"os"
+	"rand"
+	"time"
+	"fmt"
 )
 
 var draw *gdk.GdkDrawable
@@ -14,112 +14,122 @@ var gc *gdk.GdkGC
 var drawingarea *gtk.GtkDrawingArea
 
 const W = 6
+
 var H float64 = 5
+
 const MAX_Y = 900
 const MAX_X = 1100
 
 type Rect struct {
-  x, y float64
-  h, w float64
+	x, y float64
+	h, w float64
 }
 
 func (r *Rect) Draw(filled bool) {
-  draw.DrawRectangle(gc, filled, conv_x(r.x), conv_y(r.y + r.h), 
-    conv_w(r.w), conv_h(r.h))
+	if 0 == conv_h(r.h) {
+		draw.DrawRectangle(gc, filled, conv_x(r.x), conv_y(r.y+r.h),
+			conv_w(r.w), 1)
+		return
+	}
+	draw.DrawRectangle(gc, filled, conv_x(r.x), conv_y(r.y+r.h),
+		conv_w(r.w), conv_h(r.h))
 }
 
 func conv_y(y float64) int {
-  return int(((H - y) / H) * MAX_Y)
+	return int(((H - y) / H) * MAX_Y)
 }
 
 func conv_x(x float64) int {
-  return int((x / W) * MAX_X)
+	return int((x / W) * MAX_X)
 }
 
 func conv_h(h float64) int {
-  return int((h / H) * MAX_Y)
+	return int((h / H) * MAX_Y)
 }
 
 func conv_w(w float64) int {
-  return int((w / W) * MAX_X)
+	return int((w / W) * MAX_X)
 }
 
 func draw_all(a []Rect) {
-  global := Rect{0, 0, H, 1}
-  global.Draw(false)
-  for _, r := range(a) {
-    r.Draw(true)
-  }
+	global := Rect{0, 0, H, 1}
+	global.Draw(false)
+	for _, r := range a {
+		r.Draw(true)
+	}
 }
 
 func GenerateRectangles(n int) []Rect {
-  res := make([]Rect, n)
-  for y := 0; y < n; y++ {
-    res[y].h = rand.Float64()
-    res[y].w = rand.Float64()
-    res[y].x = rand.Float64()
-    res[y].y = rand.Float64()
-  }
-  return res
+	res := make([]Rect, n)
+	for y := 0; y < n; y++ {
+		res[y].h = rand.Float64()
+		res[y].w = rand.Float64()
+		res[y].x = rand.Float64()
+		res[y].y = rand.Float64()
+	}
+	return res
 }
 
 func TotalArea(rects []Rect) float64 {
-  var res float64 = 0
-  for _, r := range(rects) {
-    res += r.w * r.h
-  }
-  return res
+	var res float64 = 0
+	for _, r := range rects {
+		res += r.w * r.h
+	}
+	return res
 }
 
 func main() {
-  rand.Seed(time.Nanoseconds())
-  rects := GenerateRectangles(1000000)
-  var algo Kp1Algo
-  H = algo.Pack(rects, 0)
-  fmt.Printf("H = %0.9v\nS = %0.9v", H, TotalArea(rects))
-  return
+	rand.Seed(time.Nanoseconds())
+	rects := GenerateRectangles(5000)
+	var algo Kp1Algo
+	H = algo.Pack(rects, 0)
+	fmt.Printf("H = %0.9v\nS = %0.9v", H, TotalArea(rects))
+	//return
 
-	gtk.Init(&os.Args);
-	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL);
-	window.SetTitle("GTK DrawingArea");
+	gtk.Init(&os.Args)
+	window := gtk.Window(gtk.GTK_WINDOW_TOPLEVEL)
+	window.SetTitle("GTK DrawingArea")
 	window.Connect("destroy", func() {
-		gtk.MainQuit();
-	}, nil);
+		gtk.MainQuit()
+	},
+		nil)
 
-	vbox := gtk.VBox(true, 0);
-	vbox.SetBorderWidth(5);
-	drawingarea = gtk.DrawingArea();
-  
-  var pixmap *gdk.GdkPixmap
+	vbox := gtk.VBox(true, 0)
+	vbox.SetBorderWidth(5)
+	drawingarea = gtk.DrawingArea()
+
+	var pixmap *gdk.GdkPixmap
 
 	drawingarea.Connect("configure-event", func() {
 		if pixmap != nil {
 			pixmap.Unref()
 		}
 		var allocation gtk.GtkAllocation
-		drawingarea.GetAllocation(&allocation);
+		drawingarea.GetAllocation(&allocation)
 		draw = drawingarea.GetWindow().GetDrawable()
-		pixmap = gdk.Pixmap(drawingarea.GetWindow().GetDrawable(), allocation.Width, allocation.Height, 24);
+		pixmap = gdk.Pixmap(drawingarea.GetWindow().GetDrawable(), allocation.Width, allocation.Height, 24)
 		gc = gdk.GC(pixmap.GetDrawable())
-		gc.SetRgbFgColor(gdk.Color("white"));
-		pixmap.GetDrawable().DrawRectangle(gc, true, 0, 0, -1, -1);
-		gc.SetRgbFgColor(gdk.Color("black"));
-		gc.SetRgbBgColor(gdk.Color("white"));
-	}, nil)
+		gc.SetRgbFgColor(gdk.Color("white"))
+		pixmap.GetDrawable().DrawRectangle(gc, true, 0, 0, -1, -1)
+		gc.SetRgbFgColor(gdk.Color("black"))
+		gc.SetRgbBgColor(gdk.Color("white"))
+	},
+		nil)
 
 	drawingarea.Connect("expose-event", func() {
 		if pixmap != nil {
-			drawingarea.GetWindow().GetDrawable().DrawDrawable(gc, 
-			  pixmap.GetDrawable(), 0, 0, 0, 0, -1, -1)
+			drawingarea.GetWindow().GetDrawable().DrawDrawable(gc,
+				pixmap.GetDrawable(), 0, 0, 0, 0, -1, -1)
 		}
 		draw_all(rects)
-	}, nil);
+	},
+		nil)
 
-	vbox.Add(drawingarea);
+	vbox.Add(drawingarea)
 
-	window.Add(vbox);
-	window.Maximize();
-	window.ShowAll();
+	window.Add(vbox)
+	window.Maximize()
+	window.ShowAll()
 
-	gtk.Main();
+	gtk.Main()
 }
